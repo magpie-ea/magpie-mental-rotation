4e6a6176094b9071216ec993fed2f6984b6db3c8
MalinSpaniol <mspaniol@uos.de>
2019-07-31 19:09:12 +0200

    typo in instructions.name

--- 05_views.js
+++ 05_views.js
@@ -19,11 +19,11 @@
 */
 
 // Every experiment should start with an intro view. Here you can welcome your participants and tell them what the experiment is about
-const intro = magpieViews.view_generator("intro",{
-    trials: 1,
-    name: 'intro',
-    // If you use JavaScripts Template String `I am a Template String`, you can use HTML <></> and javascript ${} inside
-    text:   `This is a sample introduction view.
+const intro = magpieViews.view_generator("intro", {
+  trials: 1,
+  name: 'intro',
+  // If you use JavaScripts Template String `I am a Template String`, you can use HTML <></> and javascript ${} inside
+  text: `This is a sample introduction view.
             <br />
             <br />
             The introduction view welcomes the participant and gives general information
@@ -31,51 +31,51 @@ const intro = magpieViews.view_generator("intro",{
             <br />
             <br />
             This is a minimal experiment with one forced choice view. It can serve as a starting point for programming your own experiment.`,
-   buttonText: 'Begin the experiment'    
+  buttonText: 'begin the experiment'
 });
 
 // For most tasks, you need instructions views
-const instructions = magpieViews.view_generator("instructions",{
-    trials: 1,
-    name: 'instrucions',
-    title: 'General Instructions',
-    text:  `This is a sample instructions view.
+const instructions = magpieViews.view_generator("instructions", {
+  trials: 1,
+  name: 'instructions',
+  title: 'General Instructions',
+  text: `This is a sample instructions view.
             <br />
             <br />
             Tell your participants what they are to do here.`,
-    buttonText: 'go to trials'
+  buttonText: 'go to trials'
 });
 
 
 // In the post test questionnaire you can ask your participants addtional questions
-const post_test = magpieViews.view_generator("post_test",{
-    trials: 1,
-    name: 'post_test',
-    title: 'Additional information',
-    text: 'Answering the following questions is optional, but your answers will help us analyze our results.'
+const post_test = magpieViews.view_generator("post_test", {
+  trials: 1,
+  name: 'post_test',
+  title: 'Additional information',
+  text: 'Answering the following questions is optional, but your answers will help us analyze our results.'
 
-    // You can change much of what appears here, e.g., to present it in a different language, as follows:
-    // buttonText: 'Weiter',
-    // age_question: 'Alter',
-    // gender_question: 'Geschlecht',
-    // gender_male: 'männlich',
-    // gender_female: 'weiblich',
-    // gender_other: 'divers',
-    // edu_question: 'Höchster Bildungsabschluss',
-    // edu_graduated_high_school: 'Abitur',
-    // edu_graduated_college: 'Hochschulabschluss',
-    // edu_higher_degree: 'Universitärer Abschluss',
-    // languages_question: 'Muttersprache',
-    // languages_more: '(in der Regel die Sprache, die Sie als Kind zu Hause gesprochen haben)',
-    // comments_question: 'Weitere Kommentare'
+  // You can change much of what appears here, e.g., to present it in a different language, as follows:
+  // buttonText: 'Weiter',
+  // age_question: 'Alter',
+  // gender_question: 'Geschlecht',
+  // gender_male: 'männlich',
+  // gender_female: 'weiblich',
+  // gender_other: 'divers',
+  // edu_question: 'Höchster Bildungsabschluss',
+  // edu_graduated_high_school: 'Abitur',
+  // edu_graduated_college: 'Hochschulabschluss',
+  // edu_higher_degree: 'Universitärer Abschluss',
+  // languages_question: 'Muttersprache',
+  // languages_more: '(in der Regel die Sprache, die Sie als Kind zu Hause gesprochen haben)',
+  // comments_question: 'Weitere Kommentare'
 });
 
 // The 'thanks' view is crucial; never delete it; it submits the results!
 const thanks = magpieViews.view_generator("thanks", {
-    trials: 1,
-    name: 'thanks',
-    title: 'Thank you for taking part in this experiment!',
-    prolificConfirmText: 'Press the button'
+  trials: 1,
+  name: 'thanks',
+  title: 'Thank you for taking part in this experiment!',
+  prolificConfirmText: 'Press the button'
 });
 
 /** trial (magpie's Trial Type Views) below
@@ -104,15 +104,15 @@ const thanks = magpieViews.view_generator("thanks", {
 
 // Here, we initialize a normal forced_choice view
 const forced_choice_2A = magpieViews.view_generator("forced_choice", {
-    // This will use all trials specified in `data`, you can user a smaller value (for testing), but not a larger value
-    trials: trial_info.forced_choice.length,
-    // name should be identical to the variable name
-    name: 'forced_choice_2A',
-    data: trial_info.forced_choice,
-    // you can add custom functions at different stages through a view's life cycle
-    // hook: {
-    //     after_response_enabled: check_response
-    // }
+  // This will use all trials specified in `data`, you can user a smaller value (for testing), but not a larger value
+  trials: trial_info.forced_choice.length,
+  // name should be identical to the variable name
+  name: 'forced_choice_2A',
+  data: trial_info.forced_choice,
+  // you can add custom functions at different stages through a view's life cycle
+  // hook: {
+  //     after_response_enabled: check_response
+  // }
 });
 
 // There are many more templates available:
